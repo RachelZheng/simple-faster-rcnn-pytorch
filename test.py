b507f4a9ba545f229d4b6ca7f1194907d5fc16c5
Xinye Zheng <zhengxinye@gmail.com>
2019-04-29 14:36:48 -0400

    debug

--- test.py
+++ test.py
@@ -14,7 +14,6 @@ from model.faster_rcnn_vgg16 import decom_vgg16
 from model.roi_module import RoIPooling2D
 from utils import array_tool as at
 from utils.config import opt
-from utils.vis_tool_new import vis_pts, vis_bbox
 from torch.utils import data as data_
 from collections import namedtuple
 from model import FasterRCNNVGG16
@@ -31,6 +30,44 @@ LossTuple = namedtuple('LossTuple',
                         'roi_cls_loss',
                         'total_loss'
                         ])
+
+def _vis_pts(img, pts, clr=(0,0,255)):
+	""" visualize points in the image
+	Args:
+		img: 3 x n x n numpy 
+		pts: l x 2 numpy 
+	Return: img
+	"""
+	# transpose (C, H, W) -> (H, W, C)
+	img_ = np.copy(img).transpose((1, 2, 0))
+	pts_ = np.round(np.copy(pts)).astype(int)
+	for pt in pts_:
+		img_ = cv2.circle(img_, (pt[1], pt[0]), 3, clr, 3)
+
+	# transpose (H, W, C) -> (C, H, W)	
+	img_ = img_.transpose((2, 0, 1))
+	return img_
+
+
+def _vis_bbox(img, bbox, labels, scores, clr=(0,255,0)):
+	""" visualize bboxes in the image
+	Args:
+		img: 3 x n x n numpy 
+		bbox: l x 4 numpy
+		labels:	l
+		scores: l
+	Return: img
+	"""
+	# transpose (C, H, W) -> (H, W, C)
+	img_ = np.copy(img).transpose((1, 2, 0))
+	bbox_ = np.round(np.copy(bbox)).astype(int)
+	for bb in bbox_:
+		img_ = cv2.rectangle(img_, (bb[1], bb[0]), (bb[3], bb[2]), clr, 3)
+
+	# transpose (H, W, C) -> (C, H, W)
+	img_ = img_.transpose((2, 0, 1))
+	return img_
+
 
 logger = Logger('./logs')
 
@@ -54,6 +91,7 @@ os.system('mkdir %s'%(folder))
 f_pts = open(os.path.join(folder, 'pts.txt'), 'w')
 f_bbox = open(os.path.join(folder, 'bbox.txt'), 'w')
 
+
 for ii, (img, points_, labels_, scale, img_name) in tqdm(enumerate(val_dataloader)):
 	pred_bboxes_, pred_labels_, pred_scores_ = faster_rcnn.predict(img, [img.shape[2:]])
 	pred_bboxes_, pred_labels_, pred_scores_ = pred_bboxes_[0], pred_labels_[0], pred_scores_[0]
@@ -68,13 +106,8 @@ for ii, (img, points_, labels_, scale, img_name) in tqdm(enumerate(val_dataloade
 	if (ii + 1) % opt.plot_every == 0 and len(pred_bboxes_) and len(pred_bboxes_):
 		ori_img_ = inverse_normalize(img)
 		# plot image with points and bboxes
-		pred_img_ = vis_pts(ori_img_, at.tonumpy(points_))
-		_bboxes, _labels, _scores = trainer.faster_rcnn.predict(
-			[ori_img_], visualize=True)
-		pred_img_ = vis_bbox(pred_img_, 
-			at.tonumpy(pred_bboxes_), 
-			at.tonumpy(pred_labels_).reshape(-1),
-			at.tonumpy(pred_scores_))
+		pred_img_ = _vis_pts(ori_img_, points_)
+		pred_img_ = _vis_bbox(pred_img_, pred_bboxes_, pred_labels_.reshape(-1), pred_scores_)
 		ipdb.set_trace()
 		cv2.imwrite(os.path.join(folder, img_name), pred_img_.transpose((2, 0, 1)))
 
