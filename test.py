f9af9e0683325ba4e51b5d44e69a79b7f81f06b5
Xinye Zheng <zhengxinye@gmail.com>
2019-04-29 21:44:38 -0400

    debug

--- test.py
+++ test.py
@@ -65,7 +65,7 @@ def _vis_bbox(img, bbox, labels, scores, clr=(0,255,0)):
 		img_ = cv2.rectangle(img_, (bb[1], bb[0]), (bb[3], bb[2]), clr, 3)
 
 	# transpose (H, W, C) -> (C, H, W)
-	img_ = img_.transpose((2, 0, 1))
+	img_ = img_.get().transpose((2, 0, 1))
 	return img_
 
 
